765108826027cf288c8cf7ccc52d885682e0805f
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-12-13 16:57:28 +0100

    MAINTENANCE

--- lib/modules/common_settings.php
+++ lib/modules/common_settings.php
@@ -3,7 +3,6 @@
 
 	class common_settings extends modules {
 		public function __construct() {
-			$this->set_section_title( 'Common Settings' );
 			$this->set_section_type('settings');
 		}
 		/**
@@ -14,15 +13,17 @@
 		 */
 		public function init() {
 			$this->get_root()->add_section( $this );
-
+			$this->set_section_title( __('API Settings', $this->get_root()->get_prefix()) );
+			$this->set_section_desc( __('Fill out the API settings and add the widget or shortcode to your site. This will show review stars on your site and in Google search engine result pages.', $this->get_root()->get_prefix()) );
+			
 			add_action( 'admin_init', array( $this, 'admin_init' ) );
 			add_action( 'init', array( $this, 'wp_init' ) );
 		}
 		private function load_settings() {
 			$this->s['api_id']	= static::$settings->create($this)
 				->set_ID('api_id')
-				->set_title( __( 'API ID', $this->get_module_name() ) )
-				->set_description( __( 'See API Username on', $this->get_module_name() ) . ' <a href="https://www.provenexpert.com/de/personalisierte-umfragelinks/" target="_blank">ProvenExpert.com</a>' )
+				->set_title( __( 'API ID', $this->get_root()->get_prefix() ) )
+				->set_description( __( 'See API Username on', $this->get_root()->get_prefix() ) . ' <a href="https://www.provenexpert.com/de/personalisierte-umfragelinks/" target="_blank">ProvenExpert.com</a>' )
 				->set_maxlength( 32 )
 				->set_minlength( 32 )
 				->set_required( true )
@@ -30,8 +31,8 @@
 
 			$this->s['api_key']	= static::$settings->create($this)
 				->set_ID('api_key')
-				->set_title( __( 'API Key', $this->get_module_name() ) )
-				->set_description( __( 'See API Key on', $this->get_module_name() ) . ' <a href="https://www.provenexpert.com/de/personalisierte-umfragelinks/" target="_blank">ProvenExpert.com</a>' )
+				->set_title( __( 'API Key', $this->get_root()->get_prefix() ) )
+				->set_description( __( 'See API Key on', $this->get_root()->get_prefix() ) . ' <a href="https://www.provenexpert.com/de/personalisierte-umfragelinks/" target="_blank">ProvenExpert.com</a>' )
 				->set_maxlength( 43 )
 				->set_minlength( 43 )
 				->set_required( true )
