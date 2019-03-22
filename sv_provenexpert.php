2818bfc3cef1cfad5a4154deaaf37015ae110c9b
dennisheiden <heiden@straightvisions.com>
2019-03-22 17:08:13 +0100

    SUB UPDATE CORE change to v.3012

--- sv_provenexpert.php
+++ sv_provenexpert.php
@@ -15,8 +15,8 @@ namespace sv_provenexpert;
 require_once('lib/core/core.php');
 
 class init extends \sv_core\core {
-    const version							= 1310;
-    const version_core_match				= 3010;
+    const version							= 3000;
+    const version_core_match				= 3012;
 
     public function __construct(){
         $this->setup(__NAMESPACE__,__FILE__);
