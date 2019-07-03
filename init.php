039495ca41e90f7aa21b22d7585d5d074544cdc1
Matthias Bathke <info@straightvisions.com>
2019-07-03 11:39:45 +0200

    MIGRATE v1.400

---
+++ init.php
@@ -0,0 +1,45 @@
+<?php
+namespace sv_provenexpert;
+
+require_once( dirname(__FILE__) . '/lib/core_plugin/core_plugin.php' );
+
+class init extends \sv_core\core_plugin {
+	const version = 1400;
+	const version_core_match = 4005;
+
+	public function load(){
+		if(!$this->setup( __NAMESPACE__, __FILE__ )){
+			return false;
+		}
+
+		$this->set_section_title( __( 'SV ProvenExpert', $this->get_root()->get_prefix() ) );
+		$this->set_section_desc( __( 'This plugin is build to show review stars retrieved via ProvenExpert on your site – additionally this enables review stars of your website’s entries in Google’s search engine result pages.', $this->get_root()->get_prefix() ) );
+		$this->set_section_privacy( '<p>
+				' . $this->get_section_title() . ' does not collect or share any data from clients or visitors.<br />
+				' . $this->get_section_title() . ' connects to the server of <a href="https://www.provenexpert.com/de/pa281/" target="_blank">ProvenExpert</a> and only sends the given API ID and API Key, to receive the rating for that account.
+			</p>' );
+	}
+	public function update_routine() {
+		if ( $this->get_previous_version() < 1005 ) {
+			$settings = $this->modules->common_settings->get_settings();
+			$options  = get_option( 'sv_proven_expert' );
+
+			foreach ( $options['basic'] as $key => $option ) {
+				if ( $key == 'API_ID' && isset( $option['value'] ) ) {
+					$settings['api_id']->run_type()->set_data( $option['value'] )->save_option();
+				} else if ( $key == 'API_KEY' && isset( $option['value'] ) ) {
+					$settings['api_key']->run_type()->set_data( $option['value'] )->save_option();
+				}
+			}
+
+			// legacy settings
+			delete_option( 'sv_proven_expert' );
+			delete_option( 'widget_sv_provenexpert_widget' );
+		}
+
+		parent::update_routine();
+	}
+}
+
+$GLOBALS[ __NAMESPACE__ ] = new init();
+$GLOBALS[ __NAMESPACE__ ]->load();
--- sv_provenexpert.php
+++ sv_provenexpert.php
@@ -1,73 +1,25 @@
 <?php
 /*
+Version: 1.4.00
 Plugin Name: SV ProvenExpert
-Plugin URI: https://straightvisions.com/
+Text Domain: sv_provenexpert
 Description: Show rating stars via ProvenExpert.com in WordPress.
-Version: 1.3.19
+Plugin URI: https://straightvisions.com/
 Author: straightvisions GmbH
 Author URI: https://straightvisions.com
-Text Domain: sv_provenexpert
 Domain Path: /languages
+License: GPL3
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
 namespace sv_provenexpert;
 
-$min_php = '7.0.0';
-$name = 'SV ProvenExpert';
+if(!class_exists('\sv_dependencies\init')){
+	require_once( 'lib/core_plugin/dependencies/sv_dependencies.php' );
+}
 
-if(version_compare( phpversion(), $min_php, '>=' )) {
-	require_once( 'lib/core/core.php' );
-	
-	class init extends \sv_core\core {
-		const version = 1319;
-		const version_core_match = 3134;
-		
-		public function __construct() {
-			if(!$this->setup( __NAMESPACE__, __FILE__ )){
-				return false;
-			}
-			
-			/**
-			 * @desc            information for the about section
-			 * @return    void
-			 * @author            Matthias Bathke
-			 * @since            1.0
-			 */
-			
-			$this->set_section_title( __( 'SV ProvenExpert', $this->get_root()->get_prefix() ) );
-			$this->set_section_desc( __( 'This plugin is build to show review stars retrieved via ProvenExpert on your site – additionally this enables review stars of your website’s entries in Google’s search engine result pages.', $this->get_root()->get_prefix() ) );
-			$this->set_section_privacy( '<p>
-				' . $this->get_section_title() . ' does not collect or share any data from clients or visitors.<br />
-				' . $this->get_section_title() . ' connects to the server of <a href="https://www.provenexpert.com/de/pa281/" target="_blank">ProvenExpert</a> and only sends the given API ID and API Key, to receive the rating for that account.
-			</p>' );
-		}
-		
-		public function update_routine() {
-			if ( $this->get_previous_version() < 1005 ) {
-				$settings = $this->modules->common_settings->get_settings();
-				$options  = get_option( 'sv_proven_expert' );
-				
-				foreach ( $options['basic'] as $key => $option ) {
-					if ( $key == 'API_ID' && isset( $option['value'] ) ) {
-						$settings['api_id']->run_type()->set_data( $option['value'] )->save_option();
-					} else if ( $key == 'API_KEY' && isset( $option['value'] ) ) {
-						$settings['api_key']->run_type()->set_data( $option['value'] )->save_option();
-					}
-				}
-				
-				// legacy settings
-				delete_option( 'sv_proven_expert' );
-				delete_option( 'widget_sv_provenexpert_widget' );
-			}
-			
-			parent::update_routine();
-		}
-	}
-	
-	$GLOBALS[ __NAMESPACE__ ] = new init();
-}else{
-	require_once('lib/core/php_version.php');
-	add_action('init', function(){
-		\deactivate_plugins(plugin_basename( __FILE__ ) );
-	});
+if ( $GLOBALS['sv_dependencies']->set_instance_name( 'SV ProvenExpert' )->check_php_version() ) {
+	require_once( dirname(__FILE__) . '/init.php' );
+} else {
+	$GLOBALS['sv_dependencies']->php_update_notification()->prevent_plugin_activation();
 }
