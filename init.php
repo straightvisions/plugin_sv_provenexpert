93a383739ae76b09e272d5ea6d9a32f3a9d9419e
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-08-14 16:47:09 +0200

    Core match update

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ if ( ! class_exists( '\sv_core\core_plugin' ) ) {
 
 class init extends \sv_core\core_plugin {
 	const version 				= 2000;
-	const version_core_match 	= 4015;
+	const version_core_match 	= 4016;
 	
 	public function load() {
 		if ( ! $this->setup( __NAMESPACE__, __FILE__ ) ) {
