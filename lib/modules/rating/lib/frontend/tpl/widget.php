d6172d77fe9e014aad1d7efc6e7a51822c165fbd
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-08-14 16:35:42 +0200

    Renamed shortcode

--- lib/modules/rating/lib/frontend/tpl/widget.php
+++ lib/modules/rating/lib/frontend/tpl/widget.php
@@ -5,4 +5,4 @@ foreach ( $instance as $key => $setting ) {
 	$settings .=  $key . '="' . $setting . '" ';
 }
 
-echo do_shortcode( '[sv_provenexpert_rating is_widget="1" ' . $settings . ']' );
+echo do_shortcode( '[sv_provenexpert is_widget="1" ' . $settings . ']' );
