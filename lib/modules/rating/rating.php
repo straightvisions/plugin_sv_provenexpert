18357e862dc477971a88949b3c118d4a98ef476e
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-08-16 13:51:37 +0200

    Added jesko data

--- lib/modules/rating/rating.php
+++ lib/modules/rating/rating.php
@@ -101,9 +101,74 @@ class rating extends modules {
 				break;
 		}
 		
-		$ratings	= $this->get_parent()->api->request_get( 'rating' )->ratings;
 		$summary 	= $this->get_parent()->api->request_get( 'rating/summary' );
-		$profile 	= $this->get_parent()->api->request_get( 'profile' )->profile;
+		
+		//@todo Below is original
+		//$profile 	= $this->get_parent()->api->request_get( 'profile' )->profile;
+		//$ratings	= $this->get_parent()->api->request_get( 'rating' )->ratings;
+		
+		//@todo Below is temporary for Jesko
+		$profile				= (object)array();
+		$profile->profileUrl	= 'https://www.provenexpert.com/de-de/strafverteidigung-fachanwalt-fuer-strafrecht-dr-baumhoefener/';
+		$profile->company		= 'Strafverteidigung: Fachanwalt für Strafrecht Dr. Baumhöfener';
+		$profile->avatarUrl		= 'https://images.provenexpert.com/7b/47/de646a924444909709d813eced38/strafverteidigung-fachanwalt-fuer-strafrecht-dr-baumhoefener_full_1565256763.jpg';
+		
+		$ratings = (object)array(
+			'random_id_1' => (object)array(
+				'created' => 1560423852,
+				'ratingValue' => 5,
+				'ratings' => (object)array(
+					'category_0' => 5,
+					'category_1' => 5,
+					'category_2' => 5,
+					'category_3' => 5,
+					'category_4' => 5,
+					'category_5' => 5,
+					'category_6' => 4.80,
+				),
+				'feedback' => 'Ich möchte mich auf diesem Wege bei Herrn Dr. Baumhöfener bedanken. Er hat mich nach einem Urteil, welches mich aus der Bahn geworfen hat, sowohl in fachlicher Hinsicht als auch auf menschlicher Ebene mehr als nur gut betreut und ist den großen Weg der Revision gemeinsam mit mir gegangen. Er war stets erreichbar und konnte mir jedes noch so kleine und wichtige Detail erläutern, dass auch ich als Laie immer auf dem aktuellen Stand war.
+Er ist mit Abstand der kompetenteste und professionellste Anwalt den ich bislang kennenlernen durfte und meine Entscheidung Herrn Dr. Baumhöfener für mein Revisionsverfahren zu verpflichten, war die beste Entscheidung die ich hätte treffen können. Ich kann nur Danke sagen.',
+			),
+			'random_id_2' => (object)array(
+				'created' => 1559646461,
+				'ratingValue' => 5,
+				'ratings' => (object)array(
+					'category_0' => 5,
+					'category_1' => 5,
+					'category_2' => 5,
+					'category_3' => 5,
+					'category_4' => 5,
+					'category_5' => 5,
+					'category_6' => 4.80,
+				),
+				'feedback' => 'Herr Dr. Baumhöfener hat mir durch seinen Riesen Einsatz ermöglicht, dass ich weiterhin eine Zukunft auf meiner Arbeit und mein Ansehen in der Öffentlichkeit nicht verloren habe.
+Durch seine ruhige und sachliche Art hat er einem die Angst genommen vor dem was vor einem steht.
+Er war immer ansprechbar bei fragen in der Strafsachen.
+Wenn man das erste mal in so eine Situation kommt hat man viele schlaflose Nächte und sieht sich schon mit einem Bein im Gefängnis.
+Durch eine geringe Geldstrafe bin ich nicht einmal vorbestraft und kann wieder in die Zukunft blicken.
+Vielen Dank!!!!',
+			),
+			'random_id_3' => (object)array(
+				'created' => 1555153661,
+				'ratingValue' => 5,
+				'ratings' => (object)array(
+					'category_0' => 5,
+					'category_1' => 5,
+					'category_2' => 5,
+					'category_3' => 5,
+					'category_4' => 5,
+					'category_5' => 5,
+					'category_6' => 4.80,
+				),
+				'feedback' => 'Herr Dr. Baumhöfener hat mich sehr kompetent beraten und die Revision juristisch zum Erfolg geführt.
+
+Er war für mich jederzeit erreichbar und auch kurzfristige Termine oder Absprachen waren problemlos möglich.
+
+Ebenso zwischenmenschlich fühlte ich mich gut aufgehoben, ehrlich beraten und verstanden.
+
+Persönlich kann ich jeden Herrn Dr. Baumhöfener empfehlen, wenn es um Revisionen geht!',
+			),
+		);
 
 		ob_start();
 		
