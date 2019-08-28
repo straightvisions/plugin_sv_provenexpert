6ce60167ef5cb6d3d7ce1c3d30171d2c9203eb59
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-08-28 10:51:21 +0200

    Updated version

--- init.php
+++ init.php
@@ -6,8 +6,8 @@ if ( ! class_exists( '\sv_core\core_plugin' ) ) {
 }
 
 class init extends \sv_core\core_plugin {
-	const version 				= 2000;
-	const version_core_match 	= 4016;
+	const version 				= 2001;
+	const version_core_match 	= 4017;
 	
 	public function load() {
 		if ( ! $this->setup( __NAMESPACE__, __FILE__ ) ) {
