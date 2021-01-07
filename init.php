6ce8d206b519d6317f7b00f09d1d2f4e33072b04
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-01-07 09:53:03 +0100

    UPDATE CORE

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ if(!class_exists('\sv_core\core_plugin')) {
 
 class init extends \sv_core\core_plugin {
 	const version = 1513;
-	const version_core_match = 5103;
+	const version_core_match = 5104;
 
 	public function load(){
 		if(!$this->setup( __NAMESPACE__, __FILE__ )){
